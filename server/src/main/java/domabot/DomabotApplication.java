4171f1984191ff5b63b5291afd3f86195d06a06f
Jean-Baptiste WITTNER <jeanbaptiste.wittner@outlook.com>
2019-11-14 21:20:39 +0100

    ref. #1 : Adding init of spring boot

---
+++ server/src/main/java/domabot/DomabotApplication.java
@@ -0,0 +1,13 @@
+package fr.bot.domabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomabotApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/domabot/DomabotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bot.domabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
